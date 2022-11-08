9ccea4fc6c4d634ea05b674e0660e7c8999037f5
Dmitry Borisov <nagasadoy@gmail.com>
2022-11-08 12:32:14 +0300

    PHP версия 7.3 1) Деструктуризация массивов теперь поддерживает присвоение по ссылкам (работает с list и []) 2)Разрешена завершающая запятая в объявлении и вызове функций

---
+++ 7.3.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\PhpSevenThree
+{
+    echo 'Деструктурирование массива поддерживает присвоение по ссылкам ';
+    echo PHP_EOL;
+
+    $data = [
+        ['id' => 1, 'name' => 'Dmitry'],
+        ['id' => 2, 'name' => 'Andrew']
+    ];
+    var_dump($data[0]['name']);
+
+    ['name' => &$name] = $data[0];
+    $name = 'Alex';
+    var_dump($data[0]['name']);
+
+    echo 'Разрешена завершающая запятая в объявлении и вызове функций';
+    echo PHP_EOL;
+    function sum(int $a, int $b,): int{
+        return $a + $b;
+    }
+
+    echo(sum(1,1,)) . PHP_EOL;
+}
